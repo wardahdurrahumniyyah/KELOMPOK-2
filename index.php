bd51c53ae502a2e5fb8a66b481748254209821f7
wardahdurrahumniyyah <78292354+wardahdurrahumniyyah@users.noreply.github.com>
2021-02-28 04:23:18 -1200

    Mengubah tampilan Navbar

--- index.php
+++ index.php
@@ -1,158 +1,128 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<head>
+   
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
 
-    <title>KELOMPOK 2</title>
-    <style>
-    /*NAVBAR*/
-      .navbar-nav{
-        padding-right: 100px;
-      }
-      .nav-item{
-        padding-right: 25px;
-      }
-      .bg-light{
-        background-color: rgba(0, 0, 0,0) !important;
-      }
-      .bg-nontrans{
-        background-color: rgba(255,255,255,255) !important;
-      }
-      
-      /*CAROUSEL*/
-      .myCaroulsel{
-        height: 100vh;
-      }
-      .carousel-item img {
-        height: 100vh;
-      }
-       /* SESSION */
-      section{
-        padding-top: 50px;
-        padding-bottom: 50px;
-      }
-      #home{
-        padding-top: 0px;
-        padding-bottom: 0px;
-      }
-      #about, #gallery, #contact{
-        height: 700px;
-      }
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
 
-    </style>
-  </head>
-  <body>
+    <title>KELOMPOK 2 (Dumnny.Cafe)</title>
+  
+ </head>
+ <body>
+   
+   <!-- PRE LOADER -->
+     <section class="preloader">
+          <div class="spinner">
 
-    <!-- Navbar -->
-    <nav id="navbar" class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+               <span class="spinner-rotate"></span>
+               
+          </div>
+     </section>
+   
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#gallery">Gallery</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#contact">Contact Us</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-  </nav>
+    <!-- MENU -->
+     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
 
-  <section id="home">
-    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="img/1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="img/2.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="img/3.jpg" class="d-block w-100" alt="...">
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel"  data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#myCarousel"  data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-  </div>
-  </section>
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
 
-  <section id="about" class="bg-secondary">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 mx-auto text-center">
-          <h1>This is about page</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe non recusandae molestiae qui corporis tempore facere nisi blanditiis, a, ipsum ab! Earum fuga deleniti magni, reiciendis at ab exercitationem saepe.</p>
+                    <!-- lOGO TEXT HERE -->
+                    <a href="index.html" class="navbar-brand">Dumnny <span>.</span> Cafe</a>
+               </div>
 
-          <br/></br>
-          <button type="buton" class="btn btn-primary btn-lg">Button</button>
-        </div>
-      </div>
-    </div>
-  </section>
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-nav-first">
+                         <li><a href="#home" class="smoothScroll">Home</a></li>
+                         <li><a href="#about" class="smoothScroll">About</a></li>
+                         <li><a href="#team" class="smoothScroll">Chef</a></li>
+                         <li><a href="#menu" class="smoothScroll">Menu</a></li>
+                         <li><a href="#contact" class="smoothScroll">Contact</a></li>
+                    </ul>
 
-  <section id="gallery" class="bg-success">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 mx-auto text-center">
-          <h1>This is gallery page</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe non recusandae molestiae qui corporis tempore facere nisi blanditiis, a, ipsum ab! Earum fuga deleniti magni, reiciendis at ab exercitationem saepe.</p>
+                    <ul class="nav navbar-nav navbar-right">
+                         <a href="#footer" class="section-btn">Reserve a table</a>
+                    </ul>
+               </div>
 
-          <br/></br>
-          <button type="buton" class="btn btn-primary btn-lg">Button</button>
-        </div>
-      </div>
-    </div>
-  </section>
+          </div>
+     </section>
 
-  <section id="contact" class="bg-warning">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 mx-auto text-center">
-          <h1>This is contact page</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe non recusandae molestiae qui corporis tempore facere nisi blanditiis, a, ipsum ab! Earum fuga deleniti magni, reiciendis at ab exercitationem saepe.</p>
 
-          <br/></br>
-          <button type="buton" class="btn btn-primary btn-lg">Button</button>
-        </div>
-      </div>
-    </div>
-  </section>
+     <!-- HOME -->
+     <section id="home" class="slider" data-stellar-background-ratio="0.5">
+          <div class="row">
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+                    <div class="owl-carousel owl-theme">
+                         <div class="item item-first">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-8 col-sm-12">
+                                             <h3>Dumnny Cafe &amp; Restaurant</h3>
+                                             <h1>Misi kami adalah memberikan pengalaman yang tak terlupakan</h1>
+                                             <a href="#team" class="section-btn btn btn-default smoothScroll">Meet our chef</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
 
-    <script>
-      $(document).scroll(function(){
-        if ($(window).scrollTop() > 50) {
-          $('#navbar').removeClass('bg-light').addClass('bg-nontrans');
-        }else if($(window).scrollTop() <=50){
-          $('#navbar').removeClass('bg-nontrans').addClass('bg-light');
-        }
-      });
-    </script>
+                         <div class="item item-second">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-8 col-sm-12">
+                                             <h3>Sarapan Sempurna Anda</h3>
+                                             <h1>kualitas makanan terbaik juga bisa ada di sini !!</h1>
+                                             <a href="#menu" class="section-btn btn btn-default smoothScroll">Discover menu</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
 
-  </body>
+                         <div class="item item-third">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-8 col-sm-12">
+                                             <h3>Restaurant baru di kota</h3>
+                                             <h1>Nikmati menu spesial kami setiap hari Minggu dan Jumat</h1>
+                                             <a href="#contact" class="section-btn btn btn-default smoothScroll">Reservation</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+          </div>
+     </section>
+
+
+ 
+
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>  
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+     <!-- SCRIPTS -->
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/jquery.stellar.min.js"></script>
+     <script src="js/wow.min.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/jquery.magnific-popup.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
+</body>
 </html>
+
+    
