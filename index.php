411cd0f33c8baa6c85315e99d2ffc1fe32aed998
wardahdurrahumniyyah <wardahdurrah12@gmail.com>
2021-03-06 15:58:00 -1200

    mengubah about

--- index.php
+++ index.php
@@ -116,12 +116,12 @@
                          <div class="about-info">
                               <div class="section-title wow fadeInUp" data-wow-delay="0.2s">
                                    <h4>Read our story</h4>
-                                   <h2>We've been Making The Delicious Foods Since 1999</h2>
+                                   <h2>Dumnny Food 2021</h2>
                               </div>
 
                               <div class="wow fadeInUp" data-wow-delay="0.4s">
-                                   <p>Fusce hendrerit malesuada lacinia. Donec semper semper sem vitae malesuada. Proin scelerisque risus et ipsum semper molestie sed in nisi. Ut rhoncus congue lectus, rhoncus venenatis leo malesuada id.</p>
-                                   <p>Sed elementum vel felis sed scelerisque. In arcu diam, sollicitudin eu nibh ac, posuere tristique magna. You can use this template for your cafe or restaurant website. Please tell your friends about <a href="https://plus.google.com/+templatemo" target="_parent">templatemo</a>. Thank you.</p>
+                                   <p>Weekend emang paling seru makan rame-rame dengan Party Pack! Nikmati serunya Weekendmu ditemani dengan Party Pack dari @dumnny.cafe, Semua menu favorit kamu ada dalam 1 Box!</p>
+                                   <p>Jangan lupa untuk follow @dumnny.cafe untuk selalu dapetin info promo menarik lainnya juga yah. Yuk, langsung aja cek aplikasi Grabfood atau Gofood kamu untuk tahu lokasi terdekatmu ya!</p>
                               </div>
                          </div>
                     </div>
